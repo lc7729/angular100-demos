2e44b16ead7044175a674ab8803d69da394d11a5
Judy Lipinski <judy@karmoxie.com>
2019-03-16 16:54:46 -0400

    added product module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { PersonComponent } from './person/person.component';
+import { PersonNameComponent } from './person-name/person-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleListComponent
+    PeopleListComponent,
+    PersonComponent,
+    PersonNameComponent
   ],
   imports: [
     BrowserModule,
