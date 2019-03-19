0c72743197fe43f619c4ea09faf3b88be1a0e983
Judy Lipinski <judy@karmoxie.com>
2019-03-19 17:08:16 -0400

    inlined template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Welcome to {{title}}</h1>
+          <p>The year is {{theYear}}</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular100-demos';
+  isTrue = true;
+  theYear = 2019;
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PeopleListComponent } from './people-list/people-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PeopleListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
