584de50bd901dbb9851bae050310615b7199ce03
Judy Lipinski <judy@karmoxie.com>
2019-03-20 19:11:46 -0400

    added favicon changed data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}</h1>
-          <p>The year is {{theYear}}</p>`,
+  template: `
+          <h1>Welcome to {{title}}</h1>
+          <p>The year is {{theYear}}</p>
+          <p>Next year is {{1 + theYear}}</p>
+          <div [hidden]="isTrue">Is this hidden?</div>
+          `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular100-demos';
+  title: string = 'angular100-demos';
   isTrue = true;
-  theYear = 2019;
-}
+  theYear: number = 2019;
 
+  constructor() {
+    //this.theYear = new Date().getFullYear();
+  }
+
+}
