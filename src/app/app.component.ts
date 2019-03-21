f6578a7547acb27168f34ba178f998cc1769cb98
Judy Lipinski <judy@karmoxie.com>
2019-03-21 19:11:10 -0400

    added use of Person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Person } from "./shared/models/person.model";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-          <p [hidden]="isHidden">Is it hidden?</p>
-          <img [src]="'assets/favicon.png'" />
-          `,
-  styleUrls: ['./app.component.css']
+    <p [hidden]="isHidden">Is it hidden?</p>
+    <img [src]="'assets/favicon.png'" />
+  `
 })
 export class AppComponent implements OnInit {
   isHidden: boolean;
+  person: Person;
 
   ngOnInit(): void {
     this.isHidden = false;
 
-    setInterval(() => { this.isHidden = !this.isHidden; }, 1000);
+    setInterval(() => {
+      this.isHidden = !this.isHidden;
+    }, 1000);
   }
 }
-
