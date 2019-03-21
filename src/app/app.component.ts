18e54f4c70075ef2b22e038ae2db9f803f358534
Judy Lipinski <judy@karmoxie.com>
2019-03-20 20:33:57 -0400

    rm later features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-          <p [hidden]="isTrue">Is it hidden?</p>
-          <img [src]="'assets/favicon.png'" />
-          `,
-  styleUrls: ['./app.component.css']
+         Check out my favicon...
+          `
 })
 export class AppComponent {
-  isTrue = false;
-  theYear: number = 2019;
 
 }
 
