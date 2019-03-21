f942b2353163f8e99b0dbb161e1edc8861c3f8db
Judy Lipinski <judy@karmoxie.com>
2019-03-20 22:07:04 -0400

    added a setInterval that changes boolean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-          <p [hidden]="isTrue">Is it hidden?</p>
+          <p [hidden]="isHidden">Is it hidden?</p>
           <img [src]="'assets/favicon.png'" />
           `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  isTrue = false;
-  theYear: number = 2019;
+export class AppComponent implements OnInit {
+  isHidden: boolean;
 
+  ngOnInit(): void {
+    this.isHidden = false;
+
+    setInterval(() => { this.isHidden = !this.isHidden; }, 1000);
+  }
 }
 
