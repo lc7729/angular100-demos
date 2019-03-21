168c4643b5d30741c8de0e730eb897739d78207d
Judy Lipinski <judy@karmoxie.com>
2019-03-20 21:51:59 -0400

    use safe operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-          <p [hidden]="isTrue">Is it hidden?</p>
-          <img [src]="'assets/favicon.png'" />
-          `,
-  styleUrls: ['./app.component.css']
+    <p>Is {{ somePerson?.name }} here?</p>
+  `,
 })
 export class AppComponent {
-  isTrue = false;
-  theYear: number = 2019;
-
+  somePerson: any;
 }
 
+
+
+// constructor() {
+//   // this.somePerson = { name: "Don" };
+//  }
