3b0752d961c6c654b98c05a76a9ef2806a838cb0
Judy Lipinski <judy@karmoxie.com>
2019-03-21 20:52:37 -0400

    added ngif

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,11 +5,12 @@ import { Product } from './shared/product';
   selector: 'app-product-list',
   template: `
     <h1>Products</h1>
-    <ul>
-      <li *ngFor="let product of products; let i = index; trackBy: trackByFunction">
+    <ul *ngIf="products">
+      <li *ngFor="let product of products; let i = index">
         {{ i + 1 }}) {{ product.description }} {{ product.quantity }}
       </li>
     </ul>
+    <div *ngIf="!products">No products yet - wait 5 seconds</div>
   `
 })
 export class ProductListComponent implements OnInit {
@@ -17,26 +18,12 @@ export class ProductListComponent implements OnInit {
   productId = 3;
 
   ngOnInit() {
-    this.products = [
-      { id: 1, description: 'cups', quantity: 30 },
-      { id: 2, description: 'spoons', quantity: 10000 },
-      { id: 3, description: 'knives', quantity: 0 }
-    ];
-
-    const timerID = setInterval(this.addProduct, 1000);
     setTimeout(() => {
-      clearInterval(timerID);
-    }, 6000);
-  }
-
-  trackByFunction(index: number, product: Product): number {
-    return product.id;
-  }
-
-  addProduct() {
-    this.products = [
-      ...this.products,
-      { id: this.productId++, description: `item${this.productId}`, quantity: this.productId }
-    ];
+      this.products = [
+        { id: 1, description: 'cups', quantity: 30 },
+        { id: 2, description: 'spoons', quantity: 10000 },
+        { id: 3, description: 'knives', quantity: 0 }
+      ];
+    }, 5000);
   }
 }
