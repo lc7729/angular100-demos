fc2ea3587069e364b50d1a5c7a4e93465879bb3b
Judy Lipinski <judy@karmoxie.com>
2019-03-22 05:02:27 -0400

    added person-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { PersonDetailComponent } from "./person/person-detail.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PersonDetailComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
