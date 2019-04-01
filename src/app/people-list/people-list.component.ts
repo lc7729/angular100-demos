061999d46e7ab8b088048a900370fd6140af9229
Unknown <Cyyke.cs@gmail.com>
2019-04-01 16:59:24 -0700

    Update PeopleList and Person Components
    Update PeopleList to have card-deck for people
    Update Person to take input and display the card

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -3,16 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-people-list',
   template: `
-    <app-person></app-person>
-    <app-person></app-person>
-    <app-person></app-person>
+    <div class="container">
+      <div class="card-deck">
+        <app-person [name]="peopleArray[0]"></app-person>
+        <app-person [name]="peopleArray[1]"></app-person>
+        <app-person [name]="peopleArray[2]"></app-person>
+      </div>
+    </div>
   `
 })
 export class PeopleListComponent implements OnInit {
+  peopleArray: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.peopleArray = ['April', 'May', 'June'];
   }
 
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-person',
-  templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  template: `
+    <div class="card" style="width: 200px;height: 300px;">
+      <div class="card-body">
+        <h4 class="card-title text-center">{{name}}</h4>
+      </div>
+    </div>
+  `
 })
 export class PersonComponent implements OnInit {
+  @Input()
+  name: string;
 
   constructor() { }
 
