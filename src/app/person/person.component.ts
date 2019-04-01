0526d608f7dbdb3f485f969021fb39b840dbdd8f
Unknown <Cyyke.cs@gmail.com>
2019-04-01 16:59:24 -0700

    Update PeopleList and Person Components
    Update PeopleList to have card-deck for people
    Update Person to take input and display the card

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-person',
-  templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  templateUrl: './person.component.html'
 })
 export class PersonComponent implements OnInit {
+  @Input()
+  name: string;
 
   constructor() { }
 
