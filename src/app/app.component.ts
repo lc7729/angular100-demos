c1b8b84ae413842be14c72a8e070d42b447b2df1
Judy Lipinski <judy@karmoxie.com>
2019-04-03 21:48:24 -0400

    made it just be a plain service not http usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from "@angular/core";
 import { PeopleService } from "./shared/people.service";
 import { Person } from "./shared/models/person.model";
-import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
   template: `
     <ul>
-      <li *ngFor="let person of (people$ | async)">
+      <li *ngFor="let person of people">
         {{ person.first_name }}
       </li>
     </ul>
   `
 })
 export class AppComponent implements OnInit {
-  people$: Observable<Person[]>;
+  people: Person[];
 
   constructor(private peopleService: PeopleService) {}
 
   ngOnInit(): void {
-    this.people$ = this.peopleService.getPeople();
+    this.people = this.peopleService.getPeople();
   }
 }
