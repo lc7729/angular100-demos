13b6bb7098f5c59a17fca80edc20ae196597952a
Jennifer Lo <jennifer.lc.lo@gmail.com>
2019-04-09 13:12:17 -0400

    Follow along teacher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
          Check out my favicon...
           `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  person: any;
 
+  ngOnInit(): void {
+    this.person = {name: 'Fred'};
+  }
 }
 
