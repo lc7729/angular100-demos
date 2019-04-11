e5ea853295a4e91b54a133e56f46214962caf5b6
Judy Lipinski <judy@karmoxie.com>
2019-04-10 22:26:44 -0400

    rm unnecessary import

--- src/app/shared/people.service.ts
+++ src/app/shared/people.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
 import { Person } from "./models/person.model";
 
 @Injectable({
